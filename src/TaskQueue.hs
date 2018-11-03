ccd706ec9100bf8004297c23f067a46fbbb5a6d2
owen <ow.diehl@gmail.com>
2018-11-03 15:05:57 -0400

    taskqueue uses time types

--- src/TaskQueue.hs
+++ src/TaskQueue.hs
@@ -15,15 +15,18 @@ import qualified Control.Concurrent      as Conc
 import qualified Control.Concurrent.MVar as MVar
 import           Data.Hashable           (Hashable)
 import qualified Data.HashPSQ            as PSQ
-import qualified Data.UnixTime           as Time
+import qualified Data.Time.Clock         as Time
 
-defaultDelay = 1000^2
+secondsToMicroSeconds :: (Num a, RealFrac a) => a -> Int
+secondsToMicroSeconds x = floor $ x * (1000^2)
+
+defaultDelay = secondsToMicroSeconds 1
 
  -- refers to TaskQueue psq putMVar (pull semaphore, pull MVar)
-data TaskQueue a = TaskQueue (MVar (PSQ.HashPSQ a Time.UnixTime ()))
+data TaskQueue a = TaskQueue (MVar (PSQ.HashPSQ a Time.UTCTime ()))
 
-push :: (Hashable a, Ord a) => TaskQueue a -> a -> Time.UnixTime -> IO ()
-push (TaskQueue lock) a scheduleTime= do
+push :: (Hashable a, Ord a) => TaskQueue a -> a -> Time.UTCTime -> IO ()
+push (TaskQueue lock) a scheduleTime = do
   q <- takeMVar lock
   let q' = PSQ.insert a scheduleTime () q
   putMVar lock q'
@@ -31,11 +34,6 @@ push (TaskQueue lock) a scheduleTime= do
 
 pop :: (Hashable a, Ord a) => TaskQueue a -> IO a
 pop (TaskQueue lock) = do
-  let waitUntil t = do
-        now <- Time.getUnixTime
-        if now > t
-          then return ()
-          else sleepUnix $ Time.diffUnixTime t now
   q <- takeMVar lock
   case PSQ.minView q of
     Nothing -> do
@@ -43,31 +41,21 @@ pop (TaskQueue lock) = do
       threadDelay defaultDelay
       pop (TaskQueue lock)
     Just (k, p, _, q') -> do
-      now <- Time.getUnixTime
+      now <- Time.getCurrentTime
       if now > p
         then do
           putMVar lock q'
           return k
         else do
           putMVar lock q
-          waitUntil p
+          threadDelay $ secondsToMicroSeconds $ Time.diffUTCTime now p
           pop (TaskQueue lock)
 
 
 mkTaskQueue:: (Hashable a, Ord a) => [a] -> IO (TaskQueue a)
 mkTaskQueue xs = do
-  start <- Time.getUnixTime
+  start <- Time.getCurrentTime
   let psq = PSQ.fromList $ map (\e -> (e, start, ())) xs
   lock <- MVar.newEmptyMVar
   putMVar lock psq
   return $ TaskQueue lock
-
-unixToMicroSeconds :: Time.UnixDiffTime -> Int
-unixToMicroSeconds (Time.UnixDiffTime seconds micro) =
-  let s = (read . show $ seconds) :: Int
-      micros = (read . show $ micro) :: Int
-  in s * (1000^2) + micros
-
-sleepUnix :: Time.UnixDiffTime -> IO ()
-sleepUnix t = do
-  threadDelay $ unixToMicroSeconds t
