0b3e7d59c2a27ddda277ca8a299a5403ad4a391a
owen <ow.diehl@gmail.com>
2018-11-04 12:35:54 -0500

    redis uses HasRedisConn typeclass

--- src/Redis.hs
+++ src/Redis.hs
@@ -2,6 +2,7 @@
 
 module Redis where
 
+import           Config                 (HasRedisConn, getRedisConn)
 import           Control.Monad          (void)
 import           Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString        as BS
@@ -32,28 +33,28 @@ instance RedisSerializable EndpointStatus
 instance RedisSerializable BS.ByteString where
   redisFmt = id
 
-cacheEndpoint :: Red.Connection -> EndpointStatus -> IO ()
-cacheEndpoint conn (EndpointStatus endpt status) = do
-  cache conn endpt status hrInSeconds
+cacheEndpoint :: HasRedisConn a => a -> EndpointStatus -> IO ()
+cacheEndpoint a (EndpointStatus endpt status) = do
+  cache a endpt status hrInSeconds
 
 cache ::
-     (RedisSerializable k, RedisSerializable v)
-  => Red.Connection
+     (HasRedisConn a, RedisSerializable k, RedisSerializable v)
+  => a
   -> k
   -> v
   -> Integer
   -> IO ()
-cache conn k v expiry = runRedis conn $
+cache a k v expiry = runRedis (getRedisConn a) $
   void $ setex (redisFmt k) expiry $ redisFmt v
 
-scanEndpoints :: Red.Connection -> IO [EndpointStatus]
-scanEndpoints conn = scanAcc conn [] Red.cursor0
+scanEndpoints :: HasRedisConn a => a -> IO [EndpointStatus]
+scanEndpoints a = scanAcc (getRedisConn a) [] Red.cursor0
 
 -- scanAcc only receives keys, so we set the default statuses to Unknown. This
 -- is intended to be used when initially populating the worker.
-scanAcc :: Red.Connection -> [EndpointStatus] -> Red.Cursor -> IO [EndpointStatus]
-scanAcc conn acc cursor =
-  runRedis conn $ do
+scanAcc :: HasRedisConn a => a -> [EndpointStatus] -> Red.Cursor -> IO [EndpointStatus]
+scanAcc a acc cursor =
+  runRedis (getRedisConn a) $ do
     res <- Red.scan cursor
     case res of
       Left _ -> return acc
@@ -62,7 +63,7 @@ scanAcc conn acc cursor =
       Right (cursor', endpts) ->
         let newEndpts = mapEndpts Unknown endpts
         in
-          liftIO $ scanAcc conn (acc ++ newEndpts) cursor'
+          liftIO $ scanAcc a (acc ++ newEndpts) cursor'
 
 mapEndpts :: Status -> [C8.ByteString] -> [EndpointStatus]
 mapEndpts status xs =
@@ -73,13 +74,13 @@ mapEndpts status xs =
   , isEndpoint e
   ]
 
-subscriber :: Red.Connection -> [BS.ByteString] -> (Red.Message -> IO Red.PubSub) -> IO ()
-subscriber conn chans callback =
-  runRedis conn $ Red.pubSub (Red.subscribe chans) callback
+subscriber :: HasRedisConn a => a -> [BS.ByteString] -> (Red.Message -> IO Red.PubSub) -> IO ()
+subscriber a chans callback =
+  runRedis (getRedisConn a) $ Red.pubSub (Red.subscribe chans) callback
 
 -- TODO: error handling
-boundedLPush :: (RedisSerializable k, RedisSerializable v) => Red.Connection ->  k -> [v] -> Integer -> IO ()
-boundedLPush conn k vs maxLength = runRedis conn $ do
+boundedLPush :: (HasRedisConn a, RedisSerializable k, RedisSerializable v) => a ->  k -> [v] -> Integer -> IO ()
+boundedLPush a k vs maxLength = runRedis (getRedisConn a) $ do
   Red.lpush (redisFmt k) (map redisFmt vs)
   Red.ltrim (redisFmt k) 0 $ maxLength - 1
   pure ()
