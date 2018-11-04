c250ec2097f62f36ac6c5e1ab9de66cb35a1d049
owen <ow.diehl@gmail.com>
2018-11-04 15:38:46 -0500

    libmain using typeclasses + env

--- src/LibMain.hs
+++ src/LibMain.hs
@@ -2,46 +2,69 @@
 
 module LibMain where
 
-import           Control.Concurrent       (threadDelay)
-import           Control.Concurrent.Async (async, link, race, wait, waitCatch,
-                                           withAsync)
-import qualified Data.ByteString          as BS
-import qualified Data.ByteString.Char8    as C8
-import           Data.Hashable            (Hashable)
-import qualified Data.Time.Clock          as Time
-import qualified Database.Redis           as Red
-import           Endpoint                 (checkEndpoint, getEndpoint,
-                                           getStatus)
-import qualified Endpoint                 as End
-import qualified Network.HTTP.Client.TLS  as TLS
-import           Redis                    (boundedLPush, deserializeRedis,
-                                           initRedis, newEndpointTopic,
-                                           scanEndpoints, subscriber)
-import qualified TaskQueue                as TQ
+import           Config                     (Env (..), HasLog (..),
+                                             HasPgConn (..), HasRedisConn (..),
+                                             HasTq (..))
+import qualified Config                     as Conf
+import           Control.Concurrent         (threadDelay)
+import           Control.Concurrent.Async   (async, link, race, wait, waitCatch,
+                                             withAsync)
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Char8      as C8
+import           Data.Hashable              (Hashable)
+import qualified Data.Time.Clock            as Time
+import qualified Database.PostgreSQL.Simple as PG
+import qualified Database.Redis             as Red
+import           Endpoint                   (checkEndpoint, getEndpoint,
+                                             getStatus)
+import qualified Endpoint                   as End
+import qualified Network.HTTP.Client.TLS    as TLS
+import qualified Postgres.Postgres          as Postgres
+import           Redis                      (boundedLPush, deserializeRedis,
+                                             initRedis, newEndpointTopic,
+                                             scanEndpoints, subscriber)
+import qualified TaskQueue                  as TQ
 
 
 
-setup :: IO ()
+setup :: IO Env
 setup = do
   manager <- TLS.newTlsManager
   TLS.setGlobalManager manager
+  tq <- TQ.mkTaskQueue []
+  pgConn <-
+    PG.connect
+      PG.defaultConnectInfo
+        { PG.connectDatabase = Postgres.endpointsDb
+        , PG.connectPassword = "secret"
+        , PG.connectHost = "127.0.0.1"
+        }
+  redisConn <- initRedis
+  return
+    Env
+      { envLog = putStrLn
+      , envTq = tq
+      , envPgConn = pgConn
+      , envRedisConn = redisConn
+      }
+
+
 
 main :: IO ()
 main = do
-  setup
-  tq <- TQ.mkTaskQueue []
-  redisConn <- initRedis
-  popFromRedis redisConn tq
+  env <- setup
+  popFromRedis env env
   -- link redis receiver to main thread
-  async (receiveNewEndpoints redisConn tq) >>= link
+  async (receiveNewEndpoints env env) >>= link
   putStrLn "looping"
-  loop tq $ \e -> do
+  loop env $ \e -> do
     status <- checkEndpoint e
-    boundedLPush redisConn (getEndpoint status) [(getStatus status)] 100
+    boundedLPush env (getEndpoint status) [(getStatus status)] 100
     print status
 
-loop :: TQ.TaskQueue End.Endpoint -> (End.Endpoint -> IO ()) -> IO ()
-loop tq fn = do
+loop :: (HasTq a) => a -> (End.Endpoint -> IO ()) -> IO ()
+loop a fn = do
+  let tq = (getTq a)
   next <- TQ.pop tq
   let enqueue = do
         now <- Time.getCurrentTime
@@ -52,16 +75,16 @@ loop tq fn = do
   enqueue
   loop tq fn
 
-popFromRedis :: Red.Connection -> TQ.TaskQueue End.Endpoint -> IO ()
-popFromRedis conn tq = do
+popFromRedis :: (HasRedisConn a, HasTq b) => a -> b -> IO ()
+popFromRedis a b = do
   now <- Time.getCurrentTime
-  let queueEndpoint (End.EndpointStatus endpoint _) = TQ.push tq endpoint now
-  endpts <- scanEndpoints conn
+  let queueEndpoint (End.EndpointStatus endpoint _) = TQ.push (getTq b) endpoint now
+  endpts <- scanEndpoints (getRedisConn a)
   mapM_ queueEndpoint endpts
 
-receiveNewEndpoints :: Red.Connection -> TQ.TaskQueue End.Endpoint -> IO ()
-receiveNewEndpoints conn tq =
-  subscriber conn [(C8.pack newEndpointTopic)] cb
+receiveNewEndpoints :: (HasRedisConn a, HasTq b) => a -> b -> IO ()
+receiveNewEndpoints a b =
+  subscriber (getRedisConn a) [(C8.pack newEndpointTopic)] cb
   where
     cb msg = do
       print msg
@@ -69,7 +92,7 @@ receiveNewEndpoints conn tq =
       let msg' = Red.msgMessage msg
           endpt = (deserializeRedis msg') :: End.Endpoint
       if End.isEndpoint endpt then
-        TQ.push tq endpt now
+        TQ.push (getTq b) endpt now
       else
         putStrLn $ "invalid endpoint: " ++ (show msg')
       mempty
