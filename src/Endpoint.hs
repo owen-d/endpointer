62f87ea61b4632c83e1048e3543c58f39a863457
owen <ow.diehl@gmail.com>
2018-11-05 09:09:17 -0500

    ergonomic show/read instances for proto/endpoint

--- src/Endpoint.hs
+++ src/Endpoint.hs
@@ -11,14 +11,36 @@ import           GHC.Generics              (Generic)
 import qualified Network.HTTP.Simple       as Simple
 import qualified Network.HTTP.Simple       as Simple
 import qualified Network.HTTP.Types.Status as HTStat
+import qualified ReadUtils                 as RU
 
-data Endpoint = Endpoint {proto :: Proto, relativeUrl :: BS.ByteString}
-  deriving (Show, Read, Eq, Ord, Generic)
+data Endpoint = Endpoint
+  { proto       :: Proto
+  , relativeUrl :: BS.ByteString
+  } deriving (Eq, Ord, Generic)
 instance Hashable Endpoint
 
+instance Show Endpoint where
+  show e = (show $ proto e) ++ (C8.unpack $ relativeUrl e)
+
+instance Read Endpoint where
+  readsPrec i str =
+    case (readsPrec i str :: [(Proto, String)]) of
+      []          -> []
+      [(p, rest)] -> [(Endpoint p $ C8.pack rest, "")]
+
 data Proto = Http | Https
-  deriving (Show, Read, Eq, Ord, Generic)
+  deriving (Eq, Ord, Generic)
 instance Hashable Proto
+
+
+instance Show Proto where
+  show Https = "https://"
+  show Http  = "http://"
+
+instance Read Proto where
+  readsPrec _ str =
+    RU.readMatch [(Http, show Http), (Https, show Https)] str
+
 
 -- will eventually want to be able to map over json returns for different endpoints
 -- however, this isn't necessary initially
@@ -43,20 +65,11 @@ data EndpointStatus = EndpointStatus
   deriving (Show, Read, Ord, Eq)
 
 isEndpoint :: Endpoint -> Bool
-isEndpoint e = isJust . Simple.parseRequest . fmtHttp $ e
-
-fmtHttp :: Endpoint -> String
-fmtHttp e =
-  let prefix =
-        case (proto e) of
-          Http  -> "http://"
-          Https -> "https://"
-   in prefix ++ (C8.unpack $ relativeUrl e)
-
+isEndpoint e = isJust . Simple.parseRequest $ show e
 
 checkEndpoint :: Endpoint.Endpoint -> IO (Endpoint.EndpointStatus)
 checkEndpoint endpoint = do
-  req <- Simple.parseRequest $ fmtHttp endpoint
+  req <- Simple.parseRequest $ show endpoint
   res <- Simple.httpLBS req
   return . (EndpointStatus endpoint) . parseStatus . Simple.getResponseStatus $
     res
